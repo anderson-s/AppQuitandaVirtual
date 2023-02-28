f119ed380d9315cb429b801f827488fe769172f6
Andeson Silva <andsilva607@gmail.com>
2023-02-28 19:18:00 -0300

    Adicionando campo de pesquisa e carrinho

--- lib/app/ui/home_ui_tab.dart
+++ lib/app/ui/home_ui_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quitanda_virtual/app/ui/colors/custom_colors.dart';
+// ignore: library_prefixes
+import 'package:badges/badges.dart' as packageBadge;
 
 class HomeUiTab extends StatelessWidget {
   const HomeUiTab({super.key});
@@ -12,18 +14,38 @@ class HomeUiTab extends StatelessWidget {
           backgroundColor: Colors.transparent,
           elevation: 0,
           actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.shopping_cart,
-                color: CustomColors.customSwatchColor,
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 15,
+                right: 15,
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  // print("Clicando...");
+                },
+                child: packageBadge.Badge(
+                  badgeContent: const Text(
+                    "2",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                  badgeStyle: packageBadge.BadgeStyle(
+                    badgeColor: CustomColors.customContrastColor,
+                  ),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: CustomColors.customSwatchColor,
+                  ),
+                ),
               ),
             ),
           ],
           title: Text.rich(
             TextSpan(
               style: const TextStyle(
-                fontSize: 20,
+                fontSize: 30,
               ),
               children: [
                 TextSpan(
@@ -44,6 +66,42 @@ class HomeUiTab extends StatelessWidget {
             ),
           ),
           centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Pesquise aqui...",
+                  hintStyle: TextStyle(
+                    color: Colors.green.shade400,
+                    fontSize: 14,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: CustomColors.customContrastColor,
+                    size: 21,
+                  ),
+                  isDense: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      60,
+                    ),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
