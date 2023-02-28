bddd3db54fbb39e96d5e88794327c73628e5b816
Andeson Silva <andsilva607@gmail.com>
2023-02-27 22:35:45 -0300

    Adicionando botões divisores e botão para criar conta

--- lib/app/ui/login_ui.dart
+++ lib/app/ui/login_ui.dart
@@ -16,65 +16,115 @@ class LoginUi extends StatelessWidget {
                 color: Colors.green,
               ),
             ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 40,
-                  horizontal: 32,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(
-                      45,
-                    ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 40,
+                horizontal: 32,
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(
+                    45,
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const CustomTextField(
-                      labelText: "Email",
-                      iconPref: Icons.email,
-                    ),
-                    const CustomTextField(
-                      labelText: "Senha",
-                      iconPref: Icons.lock,
-                      isSecret: true,
-                    ),
-                    SizedBox(
-                      height: 40,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              18,
-                            ),
-                          ),
-                          textStyle: const TextStyle(
-                            fontSize: 18,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const CustomTextField(
+                    labelText: "Email",
+                    iconPref: Icons.email,
+                  ),
+                  const CustomTextField(
+                    labelText: "Senha",
+                    iconPref: Icons.lock,
+                    isSecret: true,
+                  ),
+                  SizedBox(
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            18,
                           ),
                         ),
-                        onPressed: () {},
-                        child: const Text(
-                          "Entrar",
+                        textStyle: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Entrar",
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Esqueceu a senha?",
+                        style: TextStyle(
+                          color: Colors.red,
                         ),
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Esqueceu a senha?",
-                          style: TextStyle(
-                            color: Colors.red,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
                           ),
                         ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                          ),
+                          child: Text(
+                            "Ou",
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            18,
+                          ),
+                        ),
+                        side: const BorderSide(
+                          width: 2,
+                          color: Colors.green,
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.green,
+                        ),
+                      ),
+                      child: const Text(
+                        "Criar Conta",
                       ),
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
             )
           ],
